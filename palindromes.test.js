b1d7ad9e5ec966f452e7c9ca5cdfee853b66ad9e
Agustino Saling <agustino@saling.eu>
2019-01-06 22:26:49 +0100

    :tada: First commit

---
+++ palindromes.test.js
@@ -0,0 +1,7 @@
+const palindromes = require("./palindromes");
+
+describe("palindromes()", () => {
+  it("correctly indentifies one-word palindromes", () => {
+    expect(palindromes("madam")).toEqual(["madam"]);
+  });
+});
